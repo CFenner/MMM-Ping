5b7833a9c28ca7219f610ec6da9d37c0ab4450b8
Christopher Fenner <CFenner@users.noreply.github.com>
2018-04-08 22:20:08 +0200

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -13,29 +13,31 @@ module.exports = NodeHelper.create({
     this.lastConnection = 'never';
     this.lastStatus = null;
     this.lastStatusChange = 'never';
+    setTimeout(this.check, 5 * 60 * 1000);
+  },
+  check: function(){
+    var that = this;
+    request({
+        url: 'http://www.google.com',
+        method: 'GET'
+      }, function(error, response, body) {
+        that.connected = !error && response.statusCode == 200;
+        if (that.connected) {
+          that.lastConnection = new Date();
+        }
+        if(that.lastStatus != that.connected){
+          that.lastStatus = that.connected
+          that.lastStatusChange = new Date();
+        }
+      }
+    );
   },
   socketNotificationReceived: function(notification, payload) {
-    //console.log(notification);
     if (notification === 'PING_REQUEST') {
-      var that = this;
-      request({
-          url: 'http://www.google.com',
-          method: 'GET'
-        }, function(error, response, body) {
-          that.connected = !error && response.statusCode == 200;
-          if (that.connected) {
-            that.lastConnection = new Date();
-          }
-          if(that.lastStatus != that.connected){
-            that.lastStatus = that.connected
-            that.lastStatusChange = new Date();
-          }
-          that.sendSocketNotification('PING_RESPONSE', {
-            connected: that.connected,
-            lastStatusChange: that.lastStatusChange
-          });
-        }
-      );
+      this.sendSocketNotification('PING_RESPONSE', {
+        connected: this.connected,
+        lastStatusChange: this.lastStatusChange
+      });
     }
   }
 });
